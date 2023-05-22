6dfae692fae31384d7eff7ccc08b25aa3f41ff04
LiorHagag <67957854+LiorHagag@users.noreply.github.com>
2023-05-22 14:26:30 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Assigment2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UploadBT = new System.Windows.Forms.Button();
+            this.uploadedFile = new System.Windows.Forms.PictureBox();
+            this.PicAfterChange = new System.Windows.Forms.PictureBox();
+            this.RemoveeColor = new System.Windows.Forms.Button();
+            this.GreyBT = new System.Windows.Forms.Button();
+            this.LoadBT = new System.Windows.Forms.Button();
+            this.FileNameText = new System.Windows.Forms.TextBox();
+            this.ClearImageBT = new System.Windows.Forms.Button();
+            this.convolution = new System.Windows.Forms.Button();
+            this.LoadingLine = new System.Windows.Forms.ProgressBar();
+            this.ColorChoose = new System.Windows.Forms.ComboBox();
+            this.MouseMove = new System.Windows.Forms.Label();
+            this.ConvoluionBox = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.uploadedFile)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicAfterChange)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // UploadBT
+            // 
+            this.UploadBT.Location = new System.Drawing.Point(386, 19);
+            this.UploadBT.Margin = new System.Windows.Forms.Padding(2);
+            this.UploadBT.Name = "UploadBT";
+            this.UploadBT.Size = new System.Drawing.Size(28, 19);
+            this.UploadBT.TabIndex = 0;
+            this.UploadBT.Text = "...";
+            this.UploadBT.UseVisualStyleBackColor = true;
+            this.UploadBT.Click += new System.EventHandler(this.UploadBT_Click);
+            // 
+            // uploadedFile
+            // 
+            this.uploadedFile.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.uploadedFile.Location = new System.Drawing.Point(16, 56);
+            this.uploadedFile.Margin = new System.Windows.Forms.Padding(2);
+            this.uploadedFile.Name = "uploadedFile";
+            this.uploadedFile.Size = new System.Drawing.Size(415, 303);
+            this.uploadedFile.TabIndex = 1;
+            this.uploadedFile.TabStop = false;
+            this.uploadedFile.Click += new System.EventHandler(this.uploadedFile_Click);
+            this.uploadedFile.MouseMove += new System.Windows.Forms.MouseEventHandler(this.uploadedFile_MouseMove);
+            // 
+            // PicAfterChange
+            // 
+            this.PicAfterChange.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PicAfterChange.Location = new System.Drawing.Point(592, 56);
+            this.PicAfterChange.Margin = new System.Windows.Forms.Padding(2);
+            this.PicAfterChange.Name = "PicAfterChange";
+            this.PicAfterChange.Size = new System.Drawing.Size(415, 303);
+            this.PicAfterChange.TabIndex = 2;
+            this.PicAfterChange.TabStop = false;
+            this.PicAfterChange.Click += new System.EventHandler(this.PicAfterChange_Click);
+            // 
+            // RemoveeColor
+            // 
+            this.RemoveeColor.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.RemoveeColor.Location = new System.Drawing.Point(435, 80);
+            this.RemoveeColor.Margin = new System.Windows.Forms.Padding(2);
+            this.RemoveeColor.Name = "RemoveeColor";
+            this.RemoveeColor.Size = new System.Drawing.Size(142, 20);
+            this.RemoveeColor.TabIndex = 4;
+            this.RemoveeColor.Text = "Remove Color";
+            this.RemoveeColor.UseVisualStyleBackColor = true;
+            this.RemoveeColor.Click += new System.EventHandler(this.RemoveeColor_Click);
+            // 
+            // GreyBT
+            // 
+            this.GreyBT.BackColor = System.Drawing.Color.Snow;
+            this.GreyBT.Location = new System.Drawing.Point(435, 115);
+            this.GreyBT.Margin = new System.Windows.Forms.Padding(2);
+            this.GreyBT.Name = "GreyBT";
+            this.GreyBT.Size = new System.Drawing.Size(56, 20);
+            this.GreyBT.TabIndex = 5;
+            this.GreyBT.Text = "Grey";
+            this.GreyBT.UseVisualStyleBackColor = false;
+            this.GreyBT.Click += new System.EventHandler(this.GreyBT_Click);
+            // 
+            // LoadBT
+            // 
+            this.LoadBT.Location = new System.Drawing.Point(418, 19);
+            this.LoadBT.Margin = new System.Windows.Forms.Padding(2);
+            this.LoadBT.Name = "LoadBT";
+            this.LoadBT.Size = new System.Drawing.Size(56, 19);
+            this.LoadBT.TabIndex = 6;
+            this.LoadBT.Text = "Load";
+            this.LoadBT.UseVisualStyleBackColor = true;
+            this.LoadBT.Click += new System.EventHandler(this.LoadBT_Click);
+            // 
+            // FileNameText
+            // 
+            this.FileNameText.Location = new System.Drawing.Point(9, 19);
+            this.FileNameText.Margin = new System.Windows.Forms.Padding(2);
+            this.FileNameText.Name = "FileNameText";
+            this.FileNameText.Size = new System.Drawing.Size(365, 20);
+            this.FileNameText.TabIndex = 7;
+            this.FileNameText.TextChanged += new System.EventHandler(this.FileNameText_TextChanged);
+            // 
+            // ClearImageBT
+            // 
+            this.ClearImageBT.Location = new System.Drawing.Point(520, 115);
+            this.ClearImageBT.Margin = new System.Windows.Forms.Padding(2);
+            this.ClearImageBT.Name = "ClearImageBT";
+            this.ClearImageBT.Size = new System.Drawing.Size(56, 19);
+            this.ClearImageBT.TabIndex = 8;
+            this.ClearImageBT.Text = "Clear";
+            this.ClearImageBT.UseVisualStyleBackColor = true;
+            this.ClearImageBT.Click += new System.EventHandler(this.ClearImageBT_Click);
+            // 
+            // convolution
+            // 
+            this.convolution.Location = new System.Drawing.Point(466, 203);
+            this.convolution.Margin = new System.Windows.Forms.Padding(2);
+            this.convolution.Name = "convolution";
+            this.convolution.Size = new System.Drawing.Size(74, 20);
+            this.convolution.TabIndex = 9;
+            this.convolution.Text = "Convolution";
+            this.convolution.UseVisualStyleBackColor = true;
+            this.convolution.Click += new System.EventHandler(this.convolution_Click);
+            // 
+            // LoadingLine
+            // 
+            this.LoadingLine.Location = new System.Drawing.Point(368, 384);
+            this.LoadingLine.Margin = new System.Windows.Forms.Padding(2);
+            this.LoadingLine.Name = "LoadingLine";
+            this.LoadingLine.Size = new System.Drawing.Size(274, 19);
+            this.LoadingLine.TabIndex = 10;
+            this.LoadingLine.Click += new System.EventHandler(this.LoadingLine_Click);
+            // 
+            // ColorChoose
+            // 
+            this.ColorChoose.CausesValidation = false;
+            this.ColorChoose.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ColorChoose.FormattingEnabled = true;
+            this.ColorChoose.Location = new System.Drawing.Point(435, 56);
+            this.ColorChoose.Margin = new System.Windows.Forms.Padding(2);
+            this.ColorChoose.Name = "ColorChoose";
+            this.ColorChoose.Size = new System.Drawing.Size(143, 21);
+            this.ColorChoose.TabIndex = 12;
+            this.ColorChoose.SelectedIndexChanged += new System.EventHandler(this.ColorChoose_SelectedIndexChanged);
+            // 
+            // MouseMove
+            // 
+            this.MouseMove.AutoSize = true;
+            this.MouseMove.Location = new System.Drawing.Point(166, 384);
+            this.MouseMove.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.MouseMove.Name = "MouseMove";
+            this.MouseMove.Size = new System.Drawing.Size(119, 13);
+            this.MouseMove.TabIndex = 13;
+            this.MouseMove.Text = "Pixels under the mouse:";
+            this.MouseMove.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MouseMove_MouseMove);
+            // 
+            // ConvoluionBox
+            // 
+            this.ConvoluionBox.CausesValidation = false;
+            this.ConvoluionBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ConvoluionBox.FormattingEnabled = true;
+            this.ConvoluionBox.Location = new System.Drawing.Point(435, 179);
+            this.ConvoluionBox.Margin = new System.Windows.Forms.Padding(2);
+            this.ConvoluionBox.Name = "ConvoluionBox";
+            this.ConvoluionBox.Size = new System.Drawing.Size(143, 21);
+            this.ConvoluionBox.TabIndex = 15;
+            this.ConvoluionBox.SelectedIndexChanged += new System.EventHandler(this.ConvoluionBox_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.RemoveeColor;
+            this.ClientSize = new System.Drawing.Size(1025, 468);
+            this.Controls.Add(this.ConvoluionBox);
+            this.Controls.Add(this.MouseMove);
+            this.Controls.Add(this.ColorChoose);
+            this.Controls.Add(this.LoadingLine);
+            this.Controls.Add(this.convolution);
+            this.Controls.Add(this.ClearImageBT);
+            this.Controls.Add(this.FileNameText);
+            this.Controls.Add(this.LoadBT);
+            this.Controls.Add(this.GreyBT);
+            this.Controls.Add(this.RemoveeColor);
+            this.Controls.Add(this.PicAfterChange);
+            this.Controls.Add(this.uploadedFile);
+            this.Controls.Add(this.UploadBT);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.Text = "Pixels changer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.uploadedFile)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicAfterChange)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button UploadBT;
+        private System.Windows.Forms.PictureBox uploadedFile;
+        private System.Windows.Forms.PictureBox PicAfterChange;
+        private System.Windows.Forms.Button RemoveeColor;
+        private System.Windows.Forms.Button GreyBT;
+        private System.Windows.Forms.Button LoadBT;
+        private System.Windows.Forms.TextBox FileNameText;
+        private System.Windows.Forms.Button ClearImageBT;
+        private System.Windows.Forms.Button convolution;
+        private System.Windows.Forms.ProgressBar LoadingLine;
+        private System.Windows.Forms.ComboBox ColorChoose;
+        private System.Windows.Forms.Label MouseMove;
+        private System.Windows.Forms.ComboBox ConvoluionBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,374 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Assigment2
+{
+    public partial class Form1 : Form
+    {
+        // simple create original's file bitmap
+        protected Bitmap getOriginalPic()
+        {
+            Bitmap Originalpic = new Bitmap(FileNameText.Text);
+            return Originalpic;
+        }
+
+        //the convolution selector - based on combo box choose
+        protected double[,] getSelectedConvolutionAction(double[,] filter)
+        {
+            if (ConvoluionBox.Text == "Smooth")
+            {
+                 filter =  new double[,] { { 1,2, 1 }, {2, 4, 2 }, { 1, 2, 1 } };
+            }
+            else if (ConvoluionBox.Text == "Sharpen")
+            {
+                filter = new double[,] { { 0, -1, 0 }, { -1, 5, -1 }, { 0, -1, 0 } };
+            }
+            else if (ConvoluionBox.Text == "Vertical")
+            {
+                filter = new double[,] { { 1, 0, -1 }, { 1, 0, -1 }, { 1, 0, -1 } };
+            }
+            else if (ConvoluionBox.Text == "Horizontal")
+            {
+                filter = new double[,] { { 1,1,1 }, {0,0,0 }, { -1, -1, -1 } };
+            }
+            else 
+            {
+                clearAll();
+                filter = null;   
+            }
+
+            return filter;
+        }
+
+        //the selector - based on combo box choose
+        protected Byte getSelectedColor(Color c)
+        {
+            
+            Byte SelectedColor = 0;
+            if (ColorChoose.Text == "Blue")
+            {
+                SelectedColor = c.B;
+            }
+            else if (ColorChoose.Text == "Green")
+            {
+                SelectedColor = c.G;
+            }
+            else if (ColorChoose.Text == "Red")
+            {
+                SelectedColor = c.R;
+            }
+          
+                return SelectedColor;
+        }
+        protected void clearAll()
+        {
+            uploadedFile.Image = null;
+            PicAfterChange.Image = null;
+            FileNameText.Text = null;
+            LoadingLine.Value = LoadingLine.Minimum;
+        }
+        //constructor
+        public Form1()
+        {
+            InitializeComponent();
+            int Blue = ColorChoose.Items.Add("Blue");
+            int Green = ColorChoose.Items.Add("Green");
+            int Red = ColorChoose.Items.Add("Red");
+            int Sharpen = ConvoluionBox.Items.Add("Sharpen");
+            int Smooth = ConvoluionBox.Items.Add("Smooth");
+            int Vertical = ConvoluionBox.Items.Add("Vertical");
+            int Horizontal = ConvoluionBox.Items.Add("Horizontal");
+        }
+
+        //turn on the processBar on any button you want
+        private void addLoadProcess()
+        {
+            BackgroundWorker bw = new BackgroundWorker();
+            bw.DoWork += LoadingLine_Click;
+            bw.WorkerReportsProgress = true;
+            bw.ProgressChanged += ChangeProgressBar;
+            bw.RunWorkerAsync();
+        }
+
+        //upload img to convert
+        private void UploadBT_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image Files(*.jpeg;*.bmp;*.png;*.jpg)|*.jpeg;*.bmp;*.png;*.jpg";
+            ofd.ShowDialog();
+            FileNameText.Text = ofd.FileName;  
+        }
+
+        //load the uploaded img
+        private void LoadBT_Click(object sender, EventArgs e)
+        {
+            try { 
+            uploadedFile.Image = getOriginalPic();
+            uploadedFile.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+            catch {
+                MessageBox.Show(
+"Please Upload an Image",
+"Error",
+MessageBoxButtons.OK,
+MessageBoxIcon.Warning
+
+);
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //takes any pixel's RGB and replace it with the avarage -> makes the image grey
+        private void GreyBT_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                addLoadProcess();
+                Bitmap originalPic = getOriginalPic();
+                Bitmap d;
+                for (int i = 0; i < originalPic.Width; i++)
+                {
+                    for (int j = 0; j < originalPic.Height; j++)
+                    {
+                        Color c = originalPic.GetPixel(i, j);
+                        byte gray = (byte)((c.R + c.G + c.B) / 3);
+                        //Set the color of this pixel
+                        originalPic.SetPixel(i, j, Color.FromArgb(gray, gray, gray));
+                    }
+                }
+
+
+                PicAfterChange.Image = originalPic;
+                PicAfterChange.SizeMode = PictureBoxSizeMode.StretchImage;
+            }catch (Exception)
+            {
+                MessageBox.Show(
+"Please Load an Image",
+"GreyScale Error",
+MessageBoxButtons.OK,
+MessageBoxIcon.Warning
+
+);
+
+            }
+        }
+
+        private void FileNameText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uploadedFile_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PicAfterChange_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RemoveeColor_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                addLoadProcess();
+                Bitmap OriginalPic = getOriginalPic();
+                //the replacement color
+                Color newColor = Color.Black;
+                //the original color
+                Color actualColor;
+                Bitmap newBitmap = new Bitmap(OriginalPic.Width, OriginalPic.Height);
+
+                for (int i = 0; i < OriginalPic.Width; i++)
+                {
+                    for (int j = 0; j < OriginalPic.Height; j++)
+                    {
+                        Color pic_colors = OriginalPic.GetPixel(i, j);
+                        byte max = Math.Max(Math.Max(pic_colors.R, pic_colors.G), pic_colors.B);
+                        byte min = Math.Min(Math.Min(pic_colors.R, pic_colors.G), pic_colors.B);
+                        actualColor = OriginalPic.GetPixel(i, j);
+
+                        //identifies whether to change the specific fix or not
+                        bool replace =
+                          getSelectedColor(pic_colors) != min
+                           && (getSelectedColor(pic_colors) == max
+                           || max - getSelectedColor(pic_colors) < 8)
+                           && (max - min) > 96;
+
+                       
+                        if (replace)
+                        {
+                            newBitmap.SetPixel(i, j, newColor);
+                        }
+
+                        else
+                        {
+                            newBitmap.SetPixel(i, j, actualColor);
+                        }
+
+                    }
+                }
+                PicAfterChange.Image = newBitmap;
+                PicAfterChange.SizeMode = PictureBoxSizeMode.StretchImage;
+            }catch (Exception)
+            {
+                MessageBox.Show(
+"Please Load an Image and select a color to continue ",
+"RemoveColor Error",
+MessageBoxButtons.OK,
+MessageBoxIcon.Warning
+
+);
+                clearAll();
+            }
+        }
+
+        private void ClearImageBT_Click(object sender, EventArgs e)
+        {
+           clearAll();
+        }
+
+        private void convolution_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                addLoadProcess();
+                Bitmap originalPic = getOriginalPic();
+                Color[,] result = new Color[originalPic.Width, originalPic.Height];
+                int filterWidth = 3;
+                int filterHeight = 3;
+                int w = originalPic.Width;
+                int h = originalPic.Height;
+                double[,] filter = new double[filterWidth, filterHeight];
+                double[,] selected = getSelectedConvolutionAction(filter);
+                //kernel matrix alternative 
+                filter = selected;
+                // makes the value more accurate 
+                double factor = 1.0;
+                double bias = 0.0;
+
+                for (int x = 0; x < w; ++x)
+                {
+                    for (int y = 0; y < h; ++y)
+                    {
+                        double red = 0.0, green = 0.0, blue = 0.0;
+
+                        for (int filterX = 0; filterX < filterWidth; filterX++)
+                        {
+                            for (int filterY = 0; filterY < filterHeight; filterY++)
+                            {
+                                int imageX = (x - filterWidth / 2 + filterX + w) % w;
+                                int imageY = (y - filterHeight / 2 + filterY + h) % h;
+
+                                Color imageColor = originalPic.GetPixel(imageX, imageY);
+                                red += imageColor.R * filter[filterX, filterY];
+                                green += imageColor.G * filter[filterX, filterY];
+                                blue += imageColor.B * filter[filterX, filterY];
+                            }
+                            int r = Math.Min(Math.Max((int)(factor * red + bias), 0), 255);
+                            int g = Math.Min(Math.Max((int)(factor * green + bias), 0), 255);
+                            int b = Math.Min(Math.Max((int)(factor * blue + bias), 0), 255);
+
+                            result[x, y] = Color.FromArgb(r, g, b);
+                        }
+                    }
+                }
+
+                for (int i = 0; i < w; ++i)
+                {
+                    for (int j = 0; j < h; ++j)
+                    {
+                        originalPic.SetPixel(i, j, result[i, j]);
+                    }
+                }
+                PicAfterChange.Image = originalPic;
+                PicAfterChange.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            catch (Exception) 
+            {
+                
+                MessageBox.Show(
+"Please Load an Image and select method to continue",
+"Convolution Error",
+MessageBoxButtons.OK,
+MessageBoxIcon.Warning
+
+);
+                clearAll();
+            }
+        }
+        private void ChangeProgressBar(Object sendler, ProgressChangedEventArgs e)
+        {
+            LoadingLine.Value = e.ProgressPercentage;
+        }
+
+        private void LoadingLine_Click(object sender, EventArgs e)
+        {
+            BackgroundWorker bw = (BackgroundWorker)sender;
+            double max = 10000000;
+            for (int i = 0; i < max; i++)
+            {
+                if (i % 1000 == 0)
+                {
+                    bw.ReportProgress((int)((i / max) * 100));
+                }
+            } 
+        }
+
+        private void ColorChoose_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MouseMove_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void MouseMove_DragOver(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void MouseMove_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void MouseMove_MouseMove(object sender, MouseEventArgs e)
+        {
+
+
+        }
+
+        private void uploadedFile_MouseMove(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                Bitmap b = getOriginalPic();
+                int ImageX = e.X * b.Width / uploadedFile.Width;
+                int ImageY = e.Y * b.Height / uploadedFile.Height;
+                Color c = b.GetPixel(ImageX, ImageY);
+                MouseMove.Text = "Pixels under the mouse: " + "(" + ImageX + " , " + ImageY + ") = < " + c.R.ToString() + " ," + c.G.ToString() + " ," + c.B.ToString() + " >";
+            }
+            catch (Exception) { MouseMove.Text = "please load an image to continue"; }
+        }
+
+      
+
+        private void ConvoluionBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
